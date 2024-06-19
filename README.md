723920adf608ae603edf0966fa483e08240f70d6
mikcheal101 <hirekaanmicheal@gmail.com>
2024-06-19 14:52:37 +0100

    git flows init

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # credit-app-notification
 Micro Service to handle notification for the credit app
+
+This service takes in a notification and dispatches it via the set type destination.
+
+### Examples
+#### SMS
+```
+{
+    NotificationRecipient: "+44700939393",
+    NotificationType: SMS,
+    NotiricationSubject: "",
+    NiotificationMessage: "Hello Dear Moses".
+}
+```
+
+##### EMail
+```
+{
+    NotificationRecipient: "sampleemail@email.com",
+    NotificationType: EMAIL,
+    NotiricationSubject: "Re: Free this weekend?",
+    NiotificationMessage: "Hello Dear Moses".
+}
+```
---
+++ src/app.py
@@ -0,0 +1,36 @@
+import awsgi
+
+import sys
+
+from flask import (
+    Flask,
+    jsonify
+)
+from flask_restful import Resource, Api
+
+
+application = Flask(__name__)
+api = Api(application)
+portNumber = 5000
+
+if sys.argv.__len__() > 1:
+    portNumber = sys.argv[1]
+    
+print("System running on port: {}".format(portNumber))
+
+
+class Notification(Resource):
+    def get(self):
+        return jsonify(status = 200, message = "OK")
+
+# api.add_resource(Notification, "/version")
+# @app.route("/version")
+# def get_version():
+#         return {"version": "1.0.0"}
+
+# @app.route("/profile")
+# def get_profile():
+#         return jsonify(name="AWS Dev")
+    
+if __name__ == "__main__":
+    application.run(host="0.0.0.0", port=portNumber)
